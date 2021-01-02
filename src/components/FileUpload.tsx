7109724bae074987573be6bf2d232286c1c8bfc0
h-ueno2 <menbou1212@gmail.com>
2021-01-02 20:10:18 +0900

    add: Modalの適用

---
+++ src/components/FileUpload.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import * as firebase from "firebase/app";
+import { useDropzone } from "react-dropzone";
+
+const FileUpload = () => {
+  return (
+    <div>
+      <h1>FileUpload</h1>
+    </div>
+  );
+}
+export default FileUpload;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-modal";
+import { Button } from "@material-ui/core";
 import { app } from "src/base";
+import FileUpload from "src/components/FileUpload";
+
+Modal.setAppElement("#root");
 
 const Home = () => {
+  const ModalStyles = {
+    overlay: {
+      backgroundColor: "rgba(0, 0, 0, 0.8)",
+    },
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      padding: 0,
+      transform: "translate(-50%, -50%)",
+    },
+  };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div>
       <h2>Home Page</h2>
-      <button onClick={() => app.auth().signOut()}>Sign out</button>
+      <Button variant="outlined" onClick={handleOpenModal}>
+        Upload
+      </Button>
+      <Button variant="outlined" onClick={() => app.auth().signOut()}>
+        Sign out
+      </Button>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={handleCloseModal}
+        style={ModalStyles}
+      >
+        <FileUpload />
+      </Modal>
     </div>
   );
 };
