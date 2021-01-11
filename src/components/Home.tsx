4e8c0d838402f029c2bf7601e1cb8d6d91278f58
h-ueno2 <menbou1212@gmail.com>
2021-01-11 10:54:41 +0900

    add: アップロードされたファイルの表示（作成途中)

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "react-modal";
-import { Button } from "@material-ui/core";
+import * as firebase from "firebase";
+import "firebase/storage";
+import { Button, GridList, GridListTile } from "@material-ui/core";
 import { app } from "src/base";
 import CustomModal from "src/components/CustomModal";
 import FileUpload from "src/components/FileUpload";
@@ -9,6 +11,8 @@ Modal.setAppElement("#root");
 
 const Home = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [files, setFiles] = useState<File[]>([]);
+  const [fileUrls, setFileUrls] = useState<string[]>([]);
 
   const handleOpenModal = () => {
     setIsModalOpen(true);
@@ -18,9 +22,35 @@ const Home = () => {
     setIsModalOpen(false);
   };
 
+  const fetchImages = async () => {
+    const storageRef = app.storage().ref().child("users/common/");
+    await storageRef.list().then((res) => {
+      let urls: string[] = [];
+      // res.items.forEach((itemRef) => {
+      //   itemRef.getDownloadURL().then((url: string) => {
+      //     urls.push(url);
+      //     console.log(url);
+      //   });
+      // });
+      setFileUrls(urls);
+    });
+  };
+  useEffect(() => {
+    fetchImages();
+  });
+
+  const thumbs = fileUrls.map((url, index) => {
+    <GridListTile key={index} rows={1}>
+      <img src={url} />
+    </GridListTile>;
+  });
+
   return (
     <div>
       <h2>Home Page</h2>
+      <div>
+        <GridList cellHeight={200}>{thumbs}</GridList>
+      </div>
       <Button variant="outlined" onClick={handleOpenModal}>
         Upload
       </Button>
